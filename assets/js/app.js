691145eeab7df154d46ccc61f3be69398eded53b
DenyskoD <deniskodima1@gmail.com>
2020-07-17 22:42:42 +0300

    +JS
    +JS  (there is bag in JS --> fixed header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,52 @@
-console.log(1);
+$(function() {
+
+    // fixed header    --> nujno dodelat` , kakou-to bag
+    var header = $("#header");
+    var intro = $("#intro");
+    var introH = 0;
+    // var introH = intro.innerHeight();
+    var scrollPosition = $(window).scrollTop();
+
+    checkScroll(scrollPosition, introH);
+
+    $(window).on("scroll", function(){
+        // var introH = intro.innerHeight();
+        var introH = 0;
+        scrollPosition = $(this).scrollTop();
+        checkScroll(scrollPosition, introH);
+    });
+
+    function checkScroll(scrollPosition, introH) {
+        if(scrollPosition >= introH) { 
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }      
+
+    //smooth scroll
+    $("[data-scroll]").on("click" , function(event) {
+        event.preventDefault();
+
+        let blockID = $(this).data("scroll");
+        let elementOffset = $(blockID).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset -50
+        }, 600);
+    });
+
+
+    // nav toggle
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+
+});
